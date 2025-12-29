9b60537ef0770b64376315242855efd470b51dbf
Ronit Kedia <kediaronit.21@gmail.com>
2025-12-29 22:47:20 +0530

    feat: add custom fonts and use it in globals.css

---
+++ src/app/fonts.ts
@@ -0,0 +1,32 @@
+import localFont from 'next/font/local';
+
+export const myFont = localFont({
+  src: [
+    {
+      path: './fonts/sans.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/sans-bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/sans-heading.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/sans-light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './fonts/sans-medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+  ],
+  variable: '--sans-font',
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { myFont } from "./fonts";
 
 export const metadata: Metadata = {
   title: "Ronit Kedia",
@@ -25,9 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${myFont.variable} font-sans antialiased h-screen`}
       >
-        {children}
+          {children}
       </body>
     </html>
   );
