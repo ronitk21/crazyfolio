802021ec0cd022af67df62790cc5bd9bdaf50781
Ronit Kedia <kediaronit.21@gmail.com>
2025-12-30 15:44:14 +0530

    fix: adjust positioning of Sun icon in mode toggle component

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -44,7 +44,7 @@ export function ModeToggle() {
       onClick={toggleTheme}
       className="absolute right-3 top-3 z-50"
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Sun className="absolute h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
       <span className="sr-only">Toggle theme</span>
     </Button>
