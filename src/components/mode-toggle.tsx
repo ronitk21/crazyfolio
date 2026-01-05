f7721d792ac5c65084bb1504783750aee3058f0d
Ronit Kedia <kediaronit.21@gmail.com>
2026-01-05 21:23:10 +0530

    fix: fix broken mode toggle button in smaller screens

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -46,13 +46,7 @@ export function ModeToggle() {
   };
 
   return (
-    <Button
-      ref={buttonRef}
-      variant="ghost"
-      size="icon-sm"
-      onClick={toggleTheme}
-      className="absolute right-3 top-3"
-    >
+    <Button ref={buttonRef} variant="ghost" size="icon-sm" onClick={toggleTheme} className="z-20">
       <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
       <span className="sr-only">Toggle theme</span>
