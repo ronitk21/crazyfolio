8410c67a84c99c35ce3ab0bf9d525de26e91d576
Ronit Kedia <kediaronit.21@gmail.com>
2026-01-05 21:19:50 +0530

    fix: correct padding values in MaxWidthWrapper component

--- src/components/max-width-wrapper.tsx
+++ src/components/max-width-wrapper.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MaxWidthWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="relative mx-auto h-full max-w-2xl px-4 py-5 sm:px-10 w-full min-w-0 overflow-x-hidden">
+    <div className="relative mx-auto h-full max-w-2xl px-4.5 py-5b sm:px-10 w-full min-w-0 overflow-x-hidden">
       {children}
     </div>
   );
