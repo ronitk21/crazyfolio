4203b365cbb15fed4004569e21834c1120df4481
Ronit Kedia <kediaronit.21@gmail.com>
2026-01-05 23:57:26 +0530

    fix: update AboutSection styling and improve introductory text

--- src/app/components/about-section.tsx
+++ src/app/components/about-section.tsx
@@ -8,7 +8,7 @@ import SocialSection from './social-section';
 
 const AboutSection = () => {
   return (
-    <div className="relative py-4 pt-38 sm:pt-52">
+    <div className="relative py-4 sm:py-0.5 pt-38 sm:pt-52">
       {/* Avatar positioned relative to this container */}
       <div className="absolute top-18 left-0 z-10 flex w-full justify-between sm:top-30">
         <Avatar className="ring-muted h-36 w-36 shadow-sm ring-4 sm:h-32 sm:w-32">
@@ -41,18 +41,18 @@ const AboutSection = () => {
         </div>
 
         <div className="text-muted-foreground text my-5 flex flex-col items-start gap-4 leading-snug font-medium text-pretty sm:mt-5 sm:gap-2.5 sm:text-base">
-          <p>
+          <p className='text-pretty'>
             Hey, I&apos;m Ronit, a full stack web developer with a
             <span className="text-foreground italic"> passion for computer science. </span>I love
             creating efficient and scalable web applications that
             <span className="text-foreground italic"> solve real-world problems</span>.
           </p>
-          {/* <p>
+          <p className='hidden sm:block text-pretty'>
             I&apos;m currently <span className="text-foreground">open for work</span>{' '}
             for any kind of <span className="text-foreground">collaboration</span>,{' '}
             <span className="text-foreground">freelance</span> or{' '}
             <span className="text-foreground">full-time</span> opportunities.
-          </p> */}
+          </p>
         </div>
 
         <div className="mt-5 flex items-center gap-2 sm:gap-3">
