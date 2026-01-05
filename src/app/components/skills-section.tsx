ea7a9ab58e23695462f001a4c775032ea038da60
Ronit Kedia <kediaronit.21@gmail.com>
2026-01-05 23:39:35 +0530

    fix: correct typo in the heading of the skills section

--- src/app/components/skills-section.tsx
+++ src/app/components/skills-section.tsx
@@ -60,7 +60,7 @@ const SkillsSection = () => {
   return (
     <div className="flex w-full flex-col items-start gap-6 overflow-hidden py-3">
       <div className="flex flex-col items-start">
-        <h1 className="text-[22px] font-bold tracking-tight">Techologies _</h1>
+        <h1 className="text-[22px] font-bold tracking-tight">Technologies _</h1>
         <p className="text-muted-foreground text-[15px] font-medium">
           These are the tech stacks and tools I have worked with. But Tech stack doesn&apos;t really
           matter, I can adapt to any tech stack quickly.
